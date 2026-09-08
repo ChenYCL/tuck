f15d2ac6ef4dfe15a4ae62ca1d87415998ce666d
zero <166997982@qq.com>
2026-09-08 20:34:07 +0800

    Initial Commit

---
+++ Tuck/TuckApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuckApp.swift
+//  Tuck
+//
+//  Created by zero on 2026/9/8.
+//
+
+import SwiftUI
+
+@main
+struct TuckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
